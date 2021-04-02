825e81532238bc0e206834c413fc865ac77985c0
sirsnowy7 <cjlane204@gmail.com>
2021-04-01 21:40:57 -0600

    added shaker sort and more debug opts

--- Scripts/Array Sorter.gd
+++ Scripts/Array Sorter.gd
@@ -18,3 +18,19 @@ func bubble_sort_step(arr):
 			done = false
 		i += 1
 	return { "array": arr, "done": done }
+	
+func shaker_sort_step(arr):
+	var data = bubble_sort_step(arr)
+	arr = data.array
+	var i = arr.size() - 2
+	if data.done:
+		return data
+	var done = false
+	while i > 0:
+		if arr[i] > arr[i+1]:
+			var tx = arr[i+1]
+			arr[i+1] = arr[i]
+			arr[i] = tx
+			done = false
+		i -= 1
+	return { "array": arr, "done": done }
--- Scripts/Debug.gd
+++ Scripts/Debug.gd
@@ -4,17 +4,23 @@ onready var ArrayGenerator = $"Array Generator"
 onready var ArraySorter = $"Array Sorter"
 onready var debug_label = $"ScrollContainer/Debug Label"
 onready var time_label = $"Time Label"
+onready var size_label = $"Size Label"
+onready var size_slider = $"Size Slider"
 
 var sorting = false
 var cur_arr
 var thread
-var elapsed = 31475858000
+var elapsed = 17380 # ay
 
 func _ready():
 	pass
 
 func _process(delta):
+	# set time label
 	time_label.text = "Elapsed time: %d:%d.%d" % [elapsed/1000/60, elapsed/1000%60, elapsed%1000]
+	# update slider label
+	size_label.text = str(size_slider.value)
+	# set main label to current array content
 	if typeof(cur_arr) != 0:
 		debug_label.text = ArrayGenerator.get_str(cur_arr)
 #	if sorting:
@@ -24,22 +30,24 @@ func _process(delta):
 #		debug_label.text = ArrayGenerator.get_str(cur_arr)
 
 func _on_Test_Button_pressed():
-	var arr = ArrayGenerator.get_new_arr(1000)
-	cur_arr = arr
-	sorting = true
-	thread = Thread.new()
-	thread.start(self, "testSort", 0)
+	if typeof(thread) == 0 || !thread.is_active():
+		thread = Thread.new()
+		var arr = ArrayGenerator.get_new_arr(size_slider.value)
+		cur_arr = arr
+		sorting = true
+		thread.start(self, "testSort", 0)
 
 # d is delay
 func testSort(d):
 	var start_t = OS.get_ticks_msec()
 	while sorting:
 		OS.delay_msec(d)
-		var data = ArraySorter.bubble_sort_step(cur_arr)
+		var data = ArraySorter.shaker_sort_step(cur_arr)
 		cur_arr = data.array
 		sorting = false if data.done else true
 	var end_t = OS.get_ticks_msec()
 	elapsed = end_t - start_t
+	thread.wait_to_finish()
 
 #func _exit_tree():
 #	thread.wait_to_finish()
